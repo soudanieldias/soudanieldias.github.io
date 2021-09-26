1e4cc3c01669324cc99723d911294838e340dcb5
Daniel Dias <danieldias.b99@hotmail.com>
2021-09-26 13:27:02 -0300

    Refatora script.js

--- js/script.js
+++ js/script.js
@@ -1,23 +1,14 @@
+// INÍCIO DO ARQUIVO DE SCRIPT
 
-    function typeWriter(elemento)
-    {
-        const textoArray = elemento.innerHTML.split('');
-        elemento.innerHTML = '';
-        textoArray.forEach((letra, i) => {
-            setTimeout(() => elemento.innerHTML += letra, 70 * i);
-        });
-    }
+// Funções De uso Global
 
+const typeWriter = (element) => {
+  // Função que Imprime no seu bloco paractere por caractere, nele contido originalmente
+  const arrayText = element.innerHTML.split(''); // Separa caractere por caractere em um array
+  element.innerHTML = ''; // Limpa bloco contendo o Texto
+  arrayText
+  .forEach((word, i) => setTimeout(() => element.innerHTML += word, 70 * i));
+};
 
-    // Se estiver tendo problemas com performance, utilize o FOR loop como abaixo no local do forEach
-    // function typeWriter(elemento) {
-    //   const textoArray = elemento.innerHTML.split('');
-    //   elemento.innerHTML = '';
-    //   for(let i = 0; i < textoArray.length; i++) {
-    //     setTimeout(() => elemento.innerHTML += textoArray[i], 75 * i);
-    //   }
-  // }
-//   const titulo = document.querySelector('h1');
-const paragrafoIntro = document.querySelector('#paragrafo-intro')
-//   typeWriter(titulo);
+const paragrafoIntro = document.querySelector('#paragrafo-intro');
 typeWriter(paragrafoIntro);
